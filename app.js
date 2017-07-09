d1acfca1c45ee3600af8745886f5058f2ad2cbf2
bdcorps <bdcorps@gmail.com>
2017-07-09 16:56:23 -0400

    testing process.env

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-var multer = require('multer'),
-    bodyParser = require('body-parser'),
-    path = require('path');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var path = require('path');
 var util = require('util');
 
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 var fs = require('fs');
 
-// create a new express server
 var app = express();
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
 
-
-// serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
 app.engine('html', require('ejs').renderFile);
@@ -44,18 +31,15 @@ var storage = multer.diskStorage({
 })
 
 app.get('/', function(req, res) {
+    console.log(process.env);
     res.render('main.ejs');
 });
-
 
 app.post('/getstarted', function(req, res) {
     var upload = multer({
         storage: storage
     }).single('upl')
-
     upload(req, res, function(err) {
-
-
         if (req.file != null && req.body.imageurl != "") {
             submitURL(req.body.imageurl, res);
 
@@ -71,15 +55,12 @@ app.post('/getstarted', function(req, res) {
                 uploadedImage: 'image'
             });
         }
-
-
     })
-
 })
 
 function submitURL(url, res) {
     var visual_recognition = new VisualRecognitionV3({
-        api_key: '87b5622939ff33ac0bd7a2909a4dd10650a07e7a',
+        api_key: '16a616ca2498eb685a5b5c9bee70511cba2ae2f',
         version_date: VisualRecognitionV3.VERSION_DATE_2016_05_20
     });
 
@@ -90,17 +71,13 @@ function submitURL(url, res) {
     };
     visual_recognition.classify(params, function(err, result) {
         if (err) {
-            /*console.log(err);
-            res.send('classified ' + err);*/
             res.render('getstarted.ejs', {
-                classifyLabel: 'classified ' + err,
+                classifyLabel: err,
                 uploadedImage: 'error'
             });
         } else {
-            /*console.log(JSON.stringify(result, null, 2));
-            res.send('classified ' + JSON.stringify(result, null, 2));*/
             res.render('getstarted.ejs', {
-                classifyLabel: 'classified ' + JSON.stringify(result, null, 2),
+                classifyLabel: JSON.stringify(result, null, 2),
                 uploadedImage: url
             });
         }
@@ -108,29 +85,23 @@ function submitURL(url, res) {
 }
 
 function submitFile(res) {
-
     var visual_recognition = new VisualRecognitionV3({
         api_key: '87b5622939ff33ac0bd7a2909a4dd10650a07e7a',
         version_date: VisualRecognitionV3.VERSION_DATE_2016_05_20
     });
     var params = {
         images_file: fs.createReadStream('./uploads/upl.jpg'),
-        //url:"http://cdn2.gurl.com/wp-content/uploads/2012/11/sexist-1.jpg",
-        classifier_ids: ['emotio']
+        classifier_ids: ['emotion']
     };
     visual_recognition.classify(params, function(err, result) {
         if (err) {
-            /*console.log(err);
-            res.send('classified ' + err);*/
             res.render('getstarted.ejs', {
-                classifyLabel: 'classified ' + err,
+                classifyLabel: err,
                 uploadedImage: 'image'
             });
         } else {
-            /*console.log(JSON.stringify(result, null, 2));
-            res.send('classified ' + JSON.stringify(result, null, 2));*/
             res.render('getstarted.ejs', {
-                classifyLabel: 'classified ' + JSON.stringify(result, null, 2),
+                classifyLabel: JSON.stringify(result, null, 2),
                 uploadedImage: 'image'
             });
         }
@@ -140,7 +111,7 @@ function submitFile(res) {
 
 app.get('/getstarted', function(req, res) {
     res.render('getstarted.ejs', {
-        classifyLabel: 'none',
+        classifyLabel: 'no response',
         uploadedImage: 'image'
     });
 });
